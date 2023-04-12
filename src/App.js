ff8bf78467fa460e1ba70c4f9868f7ceb2b83caa
taki hasan <takihasan@takis-MacBook-Air.local>
2023-04-12 17:47:12 +0530

    let's see

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome to the feature branch
+          Learn React in fun way
         </a>
       </header>
     </div>
