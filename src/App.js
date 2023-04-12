7414950965a8e1ed76e6d48500732d2e4ac9608b
taki hasan <takihasan@takis-MacBook-Air.local>
2023-04-12 14:27:06 +0530

    removed the full administrator access to check whether with only full access of elastic beanstalk this thing work or not

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in Fun way
+          Learn React
         </a>
       </header>
     </div>
