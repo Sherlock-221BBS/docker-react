2416cb291a9bc983f083ef20f1290674b131cb26
taki hasan <takihasan@takis-MacBook-Air.local>
2023-04-12 17:42:22 +0530

    added the welcome message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in fun way
+          Welcome to the feature branch
         </a>
       </header>
     </div>
